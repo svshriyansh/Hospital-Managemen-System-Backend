2c865f0072ed1d1d6cc0f21d7344efe127b26924
svshriyansh <prince.vishwakarma2000@gmail.com>
2022-06-04 11:28:34 +0530

    server changes

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
-const cors  = require('cors')
-const express = require("express")
-const mongoose = require('mongoose')
-require('dotenv').config()
-const {dbuser,dbPassword} =  process.env
-const url = `mongodb+srv://${dbuser}:${dbPassword}@cluster0.gpzb5.mongodb.net/SignUp-SignIn`
-const routes = require('./routes/auth')
-const hospitalRoutes = require('./routes/hospital')
-const patientRoutes = require('./routes/patients')
-const recordRpotes = require('./routes/medicalRecords')
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const { dbuser, dbPassword } = process.env;
+const url = `mongodb+srv://${dbuser}:${dbPassword}@cluster0.gpzb5.mongodb.net/SignUp-SignIn`;
+const routes = require("./routes/auth");
+const hospitalRoutes = require("./routes/hospital");
+const patientRoutes = require("./routes/patients");
+const recordRoutes = require("./routes/medicalRecords");
 
-const app = express()
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-mongoose.connect(url,{
+mongoose
+  .connect(url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(
-    ()=>{console.log("Database connected...")},
-    err => {console.log("Error found",err);}
-)
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database connected...");
+    },
+    (err) => {
+      console.log("Error found", err);
+    }
+  );
 
-app.use('/',routes)
-app.use('/hospital',hospitalRoutes)
-app.use('/patient',patientRoutes)
-app.use('/records',recordRpotes)
+app.use("/", routes);
+app.use("/hospital", hospitalRoutes);
+app.use("/patient", patientRoutes);
+app.use("/records", recordRoutes);
 
-
-app.listen(8000,()=>{
-    console.log('Server Connected');
-})
+app.listen(8000, () => {
+  console.log("Server Connected");
+});
